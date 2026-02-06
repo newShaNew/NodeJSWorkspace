0c9b0bab820711805bffe8e09d5fa421c839f5b6
( <oncwnuL_Q5a3V1ZCR_cd_iO00bss@git.weixin.qq.com>
2026-02-06 14:05:44 +0800

    修改本地监听

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true })); // 解析urlencoded请求体
 mountRoutes(app);
 
 // 3. 启动后端服务
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`后端服务已启动，监听端口：${port}`);
   console.log(`库存接口示例：http://localhost:${port}/api/inventory/queryByParams`);
   console.log(`库存详情接口示例：http://localhost:${port}/api/inventory/queryById/1`);
